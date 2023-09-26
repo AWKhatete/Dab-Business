cb105540550e5a4b67454bd01ec64f9bbcb3d242
AWKhatete <145686639+AWKhatete@users.noreply.github.com>
2023-09-26 15:28:51 +0300

    Add files via upload

---
+++ Dab-Web/store/views.py
@@ -0,0 +1,159 @@
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+import json 
+import datetime
+from .models import *
+from django.contrib import messages
+from django.contrib.auth.models import User,auth 
+
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password:
+            user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+            user.set_password(password)
+            user.save()
+            print("success")
+            return redirect('store/login.html')
+    else:
+         return render(request, 'store/register.html')
+    
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('store')
+        else:
+            return render(request, 'store/register.html')
+    return render(request,'store/login.html')
+
+def logout_user(request):
+    auth.logout(request)
+    return redirect('store')
+
+
+def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping': False}
+        cartItems = order['get_cart_items']
+    
+    products = Product.objects.all()
+    context = {'products': products, 'cartItems': cartItems}
+    return render(request, 'store/store.html', context)
+
+
+def cart(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
+        print('cart:', cart)
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping': False}
+        cartItems = order['get_cart_items']
+
+        for i in cart:
+            cartItems += cart[i]["quantity"]
+
+            product = Product.objects.get(id=i)
+            total = (product.price * cart[i]["quantity"])
+
+            order['get_cart_total'] += total
+            order['get_cart_items'] += cart[i]["quantity"]
+        
+    context = {'items':items, 'order':order, 'cartItems': cartItems}
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping': False}
+        cartItems = order['get_cart_items']
+        
+    context = {'items':items, 'order':order, 'cartItems': cartItems}
+    return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('productId:', productId)
+    print('action:', action)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+    
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+            order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+               state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode'],
+            )
+
+    else:
+        print('User is not logged in..')
+
+    return JsonResponse('Payment complete', safe=False)
